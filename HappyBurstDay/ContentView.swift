824ade574f42e07712066dbf9cb43981e3348ce3
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-08-09 20:08:28 +0900

    Initial Commit

---
+++ HappyBurstDay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HappyBurstDay
+//
+//  Created by 조세연 on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HappyBurstDay/HappyBurstDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBurstDayApp.swift
+//  HappyBurstDay
+//
+//  Created by 조세연 on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HappyBurstDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
